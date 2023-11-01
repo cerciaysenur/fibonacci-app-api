bf595a9153481b671678caf1d9dcb8a8cab37f28
aysenurcerci <aysenur.cerci@inventanalytics.com>
2023-11-01 21:41:59 +0300

    Implement fibonacci app api

---
+++ app/fibonacci/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import FibonacciView
+
+urlpatterns = [
+    path('<int:n>', FibonacciView.as_view(), name='fibonacci'),
+]
--- app/fibonacci/views.py
+++ app/fibonacci/views.py
@@ -1,1 +1,22 @@
-# Create your views here.
+from rest_framework.views import APIView
+from fibonacci.utils.fibonacciCalculator import FibonacciCalculator
+from fibonacci.cache.redis_cache import RedisCache
+from fibonacci.serializers import FibonacciSerializer
+from fibonacci.utils.cachedFibonacci import CachedFibonacci
+from django.http import HttpResponse
+
+
+class FibonacciView(APIView):
+    def get(self, request, n):
+        serializer = FibonacciSerializer(data={'n': n})
+        if serializer.is_valid():
+            n = serializer.validated_data['n']
+            fib_calculator = FibonacciCalculator()
+            cache_instance = RedisCache()
+            cached_fib = CachedFibonacci(
+                calculator=fib_calculator,
+                cache=cache_instance
+            )
+            result = cached_fib.calculate(n)
+            return HttpResponse(str(result) + "\n")
+        return HttpResponse(serializer.errors, status=400)
