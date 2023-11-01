8f8a89822deef3852bcf6d02262f0e5aa948c5a3
aysenurcerci <aysenur.cerci@inventanalytics.com>
2023-11-01 21:42:22 +0300

    Change redis location url in settings

--- app/app/settings.py
+++ app/app/settings.py
@@ -88,12 +88,13 @@ DATABASES = {
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": "redis://redis:6379/1",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
