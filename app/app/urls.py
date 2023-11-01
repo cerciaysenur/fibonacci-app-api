145a0befb59ade5235642656f2df9105afa44e3f
aysenurcerci <aysenur.cerci@inventanalytics.com>
2023-11-02 01:30:36 +0300

    Remove unnecessary config and url

--- app/app/urls.py
+++ app/app/urls.py
@@ -17,11 +17,9 @@ from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
 )
-from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
     path(
         'api/docs/',
