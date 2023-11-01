a97def35b2124b071886c1eabd51a3cf0e2e6d3a
aysenurcerci <aysenur.cerci@inventanalytics.com>
2023-11-01 23:40:12 +0300

    Fix url

--- app/fibonacci/urls.py
+++ app/fibonacci/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import FibonacciView
 
 urlpatterns = [
-    path('<int:n>', FibonacciView.as_view(), name='fibonacci'),
+    path('<int:n>/', FibonacciView.as_view(), name='fibonacci'),
 ]
