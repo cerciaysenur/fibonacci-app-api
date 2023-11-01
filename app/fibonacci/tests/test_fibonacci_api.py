bc3b7a7901e04a0bb5cf4831ac3582b6f0499a32
aysenurcerci <aysenur.cerci@inventanalytics.com>
2023-11-01 21:48:05 +0300

    Fix fibonacci api tests

--- app/fibonacci/tests/test_fibonacci_api.py
+++ app/fibonacci/tests/test_fibonacci_api.py
@@ -15,9 +15,8 @@ class FibonacciIntegrationTest(SimpleTestCase):
     def test_valid_fibonacci_request(self):
         response = self.client.get(fibonacci_url(7))
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, 13)
+        self.assertEqual(response.content.decode(), "13\n")
 
     def test_invalid_fibonacci_request(self):
         response = self.client.get(fibonacci_url(1001))
         self.assertEqual(response.status_code, 400)
-        self.assertIn('n', response.data)
